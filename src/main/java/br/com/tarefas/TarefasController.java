f97f7da5d680991cb0194b667d6a0c7495e6aee7
Daniel Oliveira <danieloliveiradejesus2003@gmail.com>
2026-02-09 18:15:56 -0300

    criacao da estrutura do projeto

--- src/main/java/br/com/tarefas/TarefasController.java
+++ src/main/java/br/com/tarefas/TarefasController.java
@@ -1,15 +1,39 @@
 package br.com.tarefas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @RestController
 @RequestMapping("/tarefas")
 public class TarefasController {
 
+    private final List<Tarefa> listaDeTarefas = new ArrayList<>();
+
     @GetMapping
-    public String teste(){
-        return "Teste de serviço";
+    public ResponseEntity<List<Tarefa>> recuperarTarefa() {
+        return ResponseEntity.ok(this.listaDeTarefas);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> adicionarTarefa(@RequestBody Tarefa tarefa) {
+        boolean existe = this.listaDeTarefas.stream().filter(tarefaLista -> tarefaLista.getId() == tarefa.getId()).findFirst().isPresent();
+        if (existe){
+            return ResponseEntity.badRequest().body(Map.of("messagem", "Tarefa com o ID " + tarefa.getId()+" já existente na lista"));
+        }
+        this.listaDeTarefas.add(tarefa);
+        return ResponseEntity.status(HttpStatus.CREATED).body(tarefa);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tarefa> recuperarTarefa(@PathVariable Long id) {
+        Optional<Tarefa> tarefaOptional = this.listaDeTarefas.stream().filter(tarefa -> Objects.equals(tarefa.getId(), id)).findFirst();
+        if(tarefaOptional.isPresent()){
+            return ResponseEntity.ok(tarefaOptional.get());
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
